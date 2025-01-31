8db43a4b3776a8e11e7a4fc35f95e606983828d8
joshitha-30 <joshitha301@gmail.com>
2025-01-31 08:45:21 +0530

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,35 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        // Byte Data Type
+        byte byteValue = 127;
+        System.out.println("Byte Value: " + byteValue);
+
+        // Short Data Type
+        short shortValue = 32767;
+        System.out.println("Short Value: " + shortValue);
+
+        // Int Data Type
+        int intValue = 2147483647;
+        System.out.println("Int Value: " + intValue);
+
+        // Long Data Type
+        long longValue = 9223372036854775807L;
+        System.out.println("Long Value: " + longValue);
+
+        // Float Data Type
+        float floatValue = 3.14f;
+        System.out.println("Float Value: " + floatValue);
+
+        // Double Data Type
+        double doubleValue = 3.141592653589793;
+        System.out.println("Double Value: " + doubleValue);
+
+        // Char Data Type
+        char charValue = 'A';
+        System.out.println("Char Value: " + charValue);
+
+        // Boolean Data Type
+        boolean booleanValue = true;
+        System.out.println("Boolean Value: " + booleanValue);
+    }
+} 
